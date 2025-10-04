ed5f7a1e02ae4ba2e06a31f94a31d4fc8e04d6fe
aaa123xa40126-dot <aaa123xa40126@gmail.com>
2025-10-04 17:46:50 +0800

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzz95W1zI0ckPLZCNgbAGCuO_GcUYCyqU",
+  authDomain: "ebook-site-a2008.firebaseapp.com",
+  projectId: "ebook-site-a2008",
+  storageBucket: "ebook-site-a2008.firebasestorage.app",
+  messagingSenderId: "84362629557",
+  appId: "1:84362629557:web:b8d675f6ce19a1e9a8c9fc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
